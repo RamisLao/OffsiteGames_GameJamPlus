291fe6bf65a6fe4b90473b665b8e4573d22c7525
joseramon-arias <joseramon.arias28@gmail.com>
2022-10-15 08:48:36 -0500

    Added data to CardData

--- Assets/Scripts/Deckbuilding/CardData.cs
+++ Assets/Scripts/Deckbuilding/CardData.cs
@@ -1,11 +1,28 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ERarity
+{
+    Common,
+    Uncommon,
+    Rare
+}
+
 [CreateAssetMenu(menuName = "Deckbuilding/Card Data")]
 public class CardData : ScriptableObject
 {
+    [Title("Visual Identity")]
     public string Name = "Card Name";
     public Sprite Image;
     public Color Color;
+
+    [Title("Usage")]
+    public ERarity Rarity;
+    public int ManaCost = 3;
+
+    [Title("Mechanics")]
+    [Multiline(10)]
+    public string Description = "Description";
 }
